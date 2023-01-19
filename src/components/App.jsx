f8f6ce93b835b9f34a4e02b12d811671e6e48789
Serhii Markov <108286639+KomaNatoR@users.noreply.github.com>
2023-01-19 20:48:52 +0200

    www

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,56 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import React, { Component } from "react";
+import { nanoid } from 'nanoid';
+
+export class App extends Component  {
+  state = {
+    contacts: [],
+    name: ''
+  };
+
+  addContact = (e) => {
+    e.preventDefault();
+    const { name } = this.state;
+    const id = nanoid();
+    const person = { name: { name }, id: {id} };
+
+    this.setState(prevState => ({contacts: [person,...prevState.contacts]}));
+  };
+  hendleInput = (e) => {
+    this.setState({ name: e.currentTarget.value });
+  };
+
+  render() {
+    const { contacts, name } = this.state;
+
+    return (
+      <div>
+        <div>
+          <h2>Phonebook</h2>
+          <form onSubmit={this.addContact}>
+
+            <label>
+              <input
+                type="text"
+                name="name"
+                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                required
+                onChange={this.hendleInput}
+              />
+            </label>
+
+            <button type="submit">Add contact</button>
+          </form>
+        </div>
+
+        <div>
+          <h2>Contacts</h2>
+          
+          <ul>
+            {contacts.map(cont => (<li key={cont.id}>{cont.name}</li>))}
+          </ul>
+        </div>
+      </div>
+    );
+  }
 };
